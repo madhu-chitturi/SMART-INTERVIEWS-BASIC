32c89f0d19ac817496775bef3601c85ff3e2d2b5
madhu-chitturi <106515693+madhu-chitturi@users.noreply.github.com>
2022-08-28 14:48:43 +0530

    Create ARRAY_DUPLICATE_ELEMENT.c

---
+++ ARRAY_DUPLICATE_ELEMENT.c
@@ -0,0 +1,70 @@
+Find duplicate element in array
+Problem
+Submissions
+Leaderboard
+Find a duplicate element in the given array of integers. There will be only a single duplicate element in the array.
+Note: Do not use any inbuilt functions/libraries for your main logic.
+
+Input Format
+
+First line of input contains size of the array - N and second line contains the elements of the array.
+
+Constraints
+
+2 <= N <= 100
+0 <= ar[i] <= 109
+
+Output Format
+
+Print the duplicate element from the given array.
+
+Sample Input 0
+
+6
+5 4 10 9 21 10
+Sample Output 0
+
+10
+  
+  
+<============  C PROGRAM   ============>
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+    int i, j,n;
+    long long a[300];
+    scanf("%d", &n);
+        for (i = 0; i < n; i++)
+            scanf("%lld", &a[i]);
+        for( i=0; i<n; i++)
+        {
+            for(j=i+1;j<n;j++)
+            {
+                if(a[i] == a[j])
+                {
+                    printf("%lld",a[i]);
+                     break;
+                }
+             }
+        }
+    return 0;
+}
+
+
+
+
+<================ PYTHON PROGRAM ======================>
+
+n=int(input())
+arr=list(map(int,input().strip().split()))
+for i in range(n):
+    b=arr.count(arr[i])
+    if(b>=2):
+        print(arr[i])
+        break
